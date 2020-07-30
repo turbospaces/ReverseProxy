dd686a4f316a2f7a6aaa3a53b87601204db34a11
Andrey Borisov <aandrey.borisov@gmail.com>
2020-07-30 13:12:28 +0300

    simple reverse proxy which takes target from header

--- cmd.go
+++ cmd.go
@@ -4,15 +4,13 @@ import "flag"
 
 type Cmd struct {
 	bind string
-	remote string
-	ip string
+	param string
 }
 
 func parseCmd() Cmd {
 	var cmd Cmd
-	flag.StringVar(&cmd.bind, "l", "0.0.0.0:8888", "listen on ip:port")
-	flag.StringVar(&cmd.remote, "r", "http://idea.lanyus.com:80", "reverse proxy addr")
-	flag.StringVar(&cmd.ip, "ip", "", "reverse proxy addr server ip")
+	flag.StringVar(&cmd.bind, "listen", "0.0.0.0:8089", "listen on ip:port")
+	flag.StringVar(&cmd.param, "param", "_pref", "target address query param")
 	flag.Parse()
 	return cmd
 }
--- main.go
+++ main.go
@@ -1,32 +1,30 @@
 package main
 
 import (
-	"net/http"
 	"log"
+	"net/http"
 )
 
 var cmd Cmd
 var srv http.Server
 
-func StartServer(bind string, remote string)  {
-	log.Printf("Listening on %s, forwarding to %s", bind, remote)
-	h := &handle{reverseProxy: remote}
+func StartServer(bind string, param string) {
+	log.Printf("Listening on %s, query proxy header to %s", bind, param)
+	h := &handle{param: param}
 	srv.Addr = bind
 	srv.Handler = h
-	//go func() {
-		if err := srv.ListenAndServe(); err != nil {
-			log.Fatalln("ListenAndServe: ", err)
-		}
-	//}()
+	if err := srv.ListenAndServe(); err != nil {
+		log.Fatalln("ListenAndServe: ", err)
+	}
 }
 
-func StopServer()  {
-	if err := srv.Shutdown(nil) ; err != nil {
+func StopServer() {
+	if err := srv.Shutdown(nil); err != nil {
 		log.Println(err)
 	}
 }
 
 func main() {
 	cmd = parseCmd()
-	StartServer(cmd.bind, cmd.remote)
+	StartServer(cmd.bind, cmd.param)
 }
