5bead1e5968251e154bc06a641dcd65a882c2504
Andrey Borisov <aandrey.borisov@gmail.com>
2020-07-31 15:27:50 +0300

    delete redirect query param

--- handle.go
+++ handle.go
@@ -12,9 +12,8 @@ type handle struct {
 }
 
 func (this *handle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	log.Println(r.RemoteAddr + " " + r.Method + " " + r.URL.String() + " " + r.Proto + " " + r.UserAgent())
-
-	targets, _ := r.URL.Query()[this.param]
+	q := r.URL.Query()
+	targets, _ := q[this.param]
 	target := targets[0]
 
 	remote, err := url.Parse(target)
@@ -22,9 +21,14 @@ func (this *handle) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		log.Fatalln(err)
 	}
 
+	q.Del(this.param)
+
 	proxy := httputil.NewSingleHostReverseProxy(remote)
 	r.URL.Host = remote.Host
 	r.URL.Scheme = remote.Scheme
 	r.Host = remote.Host
+	r.URL.RawQuery = q.Encode()
+
+	log.Println(r.RemoteAddr + " " + r.Method + " " + r.URL.String() + " " + r.UserAgent())
 	proxy.ServeHTTP(w, r)
 }
